13bef46c64c849a36ddb0631e522b3e745ffe17e
Moises Marquez <moy2010@gmail.com>
2022-03-05 17:52:14 +0100

    Fix examples and make API key optional

--- start-meilisearch.sh
+++ start-meilisearch.sh
@@ -5,11 +5,23 @@ MEILISEARCH_VERSION=$1
 MEILISEARCH_PORT=$2
 MEILISEARCH_API_KEY=$3
 
+if [ -z "$MEILISEARCH_API_KEY" ]; then
+  echo "::group::Starting Meilisearch server"
+  echo "  - port [$MEILISEARCH_PORT]"
+  echo "  - version [$MEILISEARCH_VERSION]"
+  echo ""
+
+  docker run --name meilisearch --publish $MEILISEARCH_PORT:$MEILISEARCH_PORT --detach --volume "$(pwd)/data.ms:/data.ms" getmeili/meilisearch:$MEILISEARCH_VERSION
+  echo "::endgroup::"
+
+  return
+fi
+
 echo "::group::Starting Meilisearch server"
 echo "  - port [$MEILISEARCH_PORT]"
 echo "  - version [$MEILISEARCH_VERSION]"
 echo "  - api key [$MEILISEARCH_API_KEY]"
 echo ""
 
-docker run --name meilisearch --publish $MEILISEARCH_PORT:$MEILISEARCH_PORT -e MEILI_MASTER_KEY=$MEILISEARCH_API_KEY --detach --volume "$(pwd)/data.ms:/data.ms" getmeili/meilisearch:$MEILISEARCH_VERSION
+docker run --name meilisearch --publish $MEILISEARCH_PORT:$MEILISEARCH_PORT -e MEILI_MASTER_KEY="$MEILISEARCH_API_KEY" --detach --volume "$(pwd)/data.ms:/data.ms" getmeili/meilisearch:$MEILISEARCH_VERSION
 echo "::endgroup::"
